311c36f492e1572ca4e9bc07a12fa382e544d1c5
jannabel <jannabelramos@hotmail.com>
2020-05-23 13:52:36 -0400

    67ad0612

---
+++ 1. Numero Entero/1. Numero Entero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._Numero_Entero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jannabel Ramos Ramirez 2019-8510
+            //Crear un programa que pida al usuario un número entero y diga si es par.
+            
+           
+
+            Console.Write(" \n ~ Ingrese un numero entero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n%2==0) Console.WriteLine("\n ~ El numero es par  ~" );
+            Console.ReadKey();
+
+
+        }
+    }
+}
